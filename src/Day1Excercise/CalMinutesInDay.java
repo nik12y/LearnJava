fe00f79425aa75cd31cce7e5c9ebe94366c86f6b
nik12y <102149528+nik12y@users.noreply.github.com>
2022-03-27 21:03:58 +0530

    Delete src directory

--- src/Day1Excercise/CalMinutesInDay.java
+++
@@ -1,8 +0,0 @@
-package Day1Excercise;
-
-public class CalMinutesInDay {
-    public static void main(String[] args) {
-        int a=24*60;
-        System.out.println("Minutes in a Day " + a);
-    }
-}
